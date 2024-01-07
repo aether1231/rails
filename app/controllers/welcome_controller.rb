a4ce88867615244f73b6cd701aee6b63e1ad529c
aether1231 <tlk999@live.co.uk>
2024-01-07 12:39:39 +0000

    26. Logging to Rails Server

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,22 @@
 class WelcomeController < ApplicationController
   def home
+    some_value = true
+    
+    if some_value
+      @message = "Welcome to my homepage"
+    else
+      @message = "Hope you are having a great day"
+    end
+
+    Rails.logger.debug "Message: #{ @message}"
   end
 
   def about
   end
 
   def contact
+  end
+
+  def features
   end
 end
